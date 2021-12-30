7e0ebff8a6fc30b4874f52f6a8dea5d578f9250c
kertio <_root_@list.ru>
2021-12-30 09:56:03 +0500

    Update massa_address_remove.py

--- massa_address_remove.py
+++ massa_address_remove.py
@@ -8,49 +8,48 @@ WALLET = Path(pathlib.Path.cwd(), 'massa', 'massa-client', 'wallet.dat')
 # Удаляет из списка выбранные адреса из списка js
 # Удалять можно как по индексу так и по адресу кошелька
 # При ошибках возвращает пустой список None
-def del_address(js, idx = None, addr=None):
-    if addr == None and idx == None:
-        return js[:1]
-    
+def del_address(js, in_addr):
     try:
-        js.pop(idx) if addr == None else js.remove(addr)
-        print('Выбранный адрес кошелька удален!')
-    except Exception:
-        print('Индекс или адрес кошелька указаны неверно\n')
-   
+        if str(in_addr).isdigit():
+            js.pop(int(int(in_addr) - 1))
+        elif in_addr != '':
+            js.remove(in_addr)
+        else:
+            return js[:1]
+    except:
+        print('Не верно введен адрес или индекс кошелька')
+        return None
+
     return js
 
 # Сохранить данные из списка в файл в формате JSON
 # Файл при этом очищается, а данные записываются новые в пустой файл
 def save_addr(js, WALLET):
+    if js == None:
+        return 
+
     with open(WALLET, 'w') as f:
         json.dump(js, f)
     
     
 # Main
-if __name__ == "__main__":
+def main():
     with open(WALLET, 'r') as f:
         js = json.load(f)          
        
     if len(js) == 1:
         print("В кошельке только один адрес. Нечего удалять")
-        exit(0)    
+        return  
         
     print("Список адресов, которые были найдены в файле wallet.dat:\n")
     for i, j in enumerate(js):
         print(f"{i+1}) {j}")
+
+
     print("\nВведите индекс или адрес кошелька, который нужно удалить. (Если ничего не вводить, по умолчанию удалятся все, кроме первого)")
-    addr = input();
+    save_addr(del_address(js, input()), WALLET)
 
-    if addr == '':
-        js_ = del_address(js)
-    elif addr.isdigit():
-        js_ = del_address(js, idx = int(addr) - 1)
-    else:
-        js_ = del_address(js, addr = addr)
-
-
-    if js_ != js:
-        save_addr(js_, WALLET)
+if __name__ == "__main__":
+    main()
 
         
