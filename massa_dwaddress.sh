113872add260e0287f7bd004ac6e23119d8ddcfd
kertio <_root_@list.ru>
2021-12-31 19:48:22 +0500

    Update massa_dwaddress.sh

--- massa_dwaddress.sh
+++ massa_dwaddress.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
- wget https://raw.githubusercontent.com/kertio/massa/main/massa_address_remove.py
- chmod +x massa_address_remove.py
-./massa_address_remove.py
+wget https://raw.githubusercontent.com/kertio/massa/main/massa_address_remove.py
+chmod +x massa_address_remove.py
+python3 massa_address_remove.py
  
